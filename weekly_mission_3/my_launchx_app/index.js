744dbcceeaab3801bc15a1c7c297e3c1a4998ec8
Alejandro Gutiérrez <alexgtz.up@gmail.com>
2022-04-22 12:10:45 -0500

    Paso 2. Creación de clase y modularización

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander')
+
+const alex = new MissionCommander('Alex')
+const karen = new MissionCommander('Karenina')
+const west = new MissionCommander('Westin')
+console.log(alex.name)
+console.log(karen.name)
+console.log(west.name)
+
