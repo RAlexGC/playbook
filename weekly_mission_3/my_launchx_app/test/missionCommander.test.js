eb04d0719e543aee5db639a7e5047e24b8d246a9
Alejandro Gutiérrez <alexgtz.up@gmail.com>
2022-04-22 13:14:01 -0500

    Removiendo my

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++
@@ -1,8 +0,0 @@
-const MissionCommander = require('./../app/missionCommander')
-
-describe("Unit Test para clase Mission Commander", () => {
-    test('Creación de objeto mission commander', () => {
-        	const myMissionCommander = new MissionCommander('McFly')
-            expect(myMissionCommander.name).toBe('McFly')
-    })
-})
